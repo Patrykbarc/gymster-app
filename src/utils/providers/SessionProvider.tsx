b321cd6eaf4fe0bedb322dcbf9ecf0fb5dca8dd7
Patryk Barć <patrykbarc.design@gmail.com>
2024-09-05 20:18:55 +0200

    Extended SessionProvider of subabase auth state change, add handle logout

--- src/utils/providers/SessionProvider.tsx
+++ src/utils/providers/SessionProvider.tsx
@@ -11,12 +11,27 @@ export function SessionProvider({ children }: Children) {
     async function getUserSession() {
       console.log('Fetching session data...')
       const { data, error } = await supabase.auth.getUser()
-
-      data && setSession(data.user)
-      error && console.log('Error during fetching user session')
+      if (data) {
+        setSession(data.user)
+      } else if (error) {
+        console.log('Error during fetching user session:', error.message)
+      }
     }
 
     getUserSession()
+
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      console.log(event)
+      if (session?.user) {
+        setSession(session.user)
+      } else {
+        setSession(null)
+      }
+    })
+
+    return () => {
+      data.subscription.unsubscribe()
+    }
   }, [])
 
   return (
