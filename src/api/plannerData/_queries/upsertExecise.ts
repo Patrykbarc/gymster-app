592ffc15163d2644f1e434ef4f4c4834a0d6f7fd
Patryk Barć <patrykbarc.design@gmail.com>
2024-09-25 10:00:38 +0200

    Change implementation of remove sets from mappedExercises in upsertExercise

--- src/api/plannerData/_queries/upsertExecise.ts
+++ src/api/plannerData/_queries/upsertExecise.ts
@@ -1,14 +1,20 @@
+import { Sets } from '../../../ui/views/WorkoutForm/_types/Sets'
 import { SubmitFormWorkout } from '../../../ui/views/WorkoutForm/_types/SubmitFormWorkout'
 import { supabase } from '../../supabase'
 import { printError } from '../_helpers/printError'
 
-export async function upsertExecise(
-  workoutId: string,
-  exercises: SubmitFormWorkout['exercises']
-) {
+type Exercises = SubmitFormWorkout['exercises']
+type ExerciseCopy = {
+  workout_id: string
+  name: string
+  sets?: Sets[]
+}
+
+export async function upsertExecise(workoutId: string, exercises: Exercises) {
   const mappedExercises = exercises.map((exercise) => {
-    const { sets, ...rest } = exercise
-    return { ...rest, workout_id: workoutId }
+    const exerciseCopy: ExerciseCopy = { ...exercise, workout_id: workoutId }
+    delete exerciseCopy.sets
+    return exerciseCopy
   })
 
   const { data, error } = await supabase
