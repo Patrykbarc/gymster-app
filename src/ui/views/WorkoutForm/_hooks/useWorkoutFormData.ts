6f0d37e70475f33d84687ee80cd99fea3bf7194a
Patryk Barć <patrykbarc.design@gmail.com>
2024-09-25 08:50:31 +0200

    Add JSDoc for useWorkoutFormData custom hook

--- src/ui/views/WorkoutForm/_hooks/useWorkoutFormData.ts
+++ src/ui/views/WorkoutForm/_hooks/useWorkoutFormData.ts
@@ -4,11 +4,20 @@ import { WORKOUT_DEFAULT_VALUES } from '../_helpers/workout-default-values'
 import { SubmitFormWorkout } from '../_types/SubmitFormWorkout'
 
 type Props = {
-  watch?: UseFormWatch<SubmitFormWorkout> | undefined
+  watch: UseFormWatch<SubmitFormWorkout> | undefined
 }
 
+const STORAGE_ITEM_NAME = 'workout-draft'
+
+/**
+ * Custom hook that handles form data for a workout.
+ * This hook serves two main purposes:
+ * 1. Retrieves saved form data from `localStorage` if it exists, otherwise returns default values.
+ * 2. Subscribes to form changes (using `react-hook-form`'s `watch`), and updates `localStorage` when the form data changes.
+ */
 export function useWorkoutFormData({ watch }: Props) {
-  const savedFormData = localStorage.getItem('workoutForm')
+  const savedFormData = localStorage.getItem(STORAGE_ITEM_NAME)
+
   const defaultValues = savedFormData
     ? JSON.parse(savedFormData)
     : WORKOUT_DEFAULT_VALUES.defaultValues
@@ -17,7 +26,7 @@ export function useWorkoutFormData({ watch }: Props) {
     if (!watch) return
 
     const subscription = watch((value) => {
-      localStorage.setItem('workoutForm', JSON.stringify(value))
+      localStorage.setItem(STORAGE_ITEM_NAME, JSON.stringify(value))
     })
     return () => subscription.unsubscribe()
   }, [watch])
